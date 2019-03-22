449ce6a2eb78574739266451b2a0c200f9f5bfac
Henry Blevins <heblevi@gmail.com>
2019-03-22 13:49:58 -0400

    Improve data presentation. Add easter egg.

--- starwars/src/components/Characters/Character.js
+++ starwars/src/components/Characters/Character.js
@@ -8,14 +8,25 @@ const Character = ({character, getStateAPI}) => {
     <div className="character">
       <h2 className="name">{name}</h2>
       <div className="info-line">
-        Born: {birth_year}, Height: {height}cm, Weight: {mass}kg
+        Born: {birth_year}, Height: {height}{height!=="unknown"?"cm":""}, Mass: {mass}{mass!=="unknown"?"kg":""}
       </div>
       <div className="info">
         <p>
-          {name} is from {homeworldState ? homeworldState.name : "Loading"}. {gender === "male" ? "He" : (gender === "female" ? "She" : "They")} appeared in {films.length} film{films.length === 1 ? "" : "s"}.
+          {homeworldState
+           ? (homeworldState.name === "unknown"
+              ? `${name}'s origin is unknown`
+              : `${name} is from ${homeworldState.name}`)
+           : "Loading"}.
+          {gender === "male"
+           ? " He "
+           : (gender === "female"
+              ? " She "
+              : " They ")}
+          appeared in {films.length} film{films.length === 1 ? "" : "s"}.
+          {name === "Palpatine" ? " He is the senate." : ""}
         </p>
       </div>
-    <div></div>
+      <div></div>
     </div>
   );
 };
