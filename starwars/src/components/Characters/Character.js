ac8e78d1085640b1609af849ec36ba54721bb39a
Henry Blevins <heblevi@gmail.com>
2019-03-22 12:39:47 -0400

    Change displayed character information

--- starwars/src/components/Characters/Character.js
+++ starwars/src/components/Characters/Character.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "./Character.scss";
 
 const Character = ({character, getStateAPI}) => {
-  const { name, birth_year, homeworld } = character;
+  const { name, birth_year, height, mass, homeworld, gender, films } = character;
   const homeworldState = getStateAPI("planets", homeworld);
   return (
     <div className="character">
-      <h2>{name}</h2>
-      <h3>{birth_year}</h3>
-    <div>{homeworldState ? homeworldState.name : "Loading"}</div>
+      <h2 className="name">{name}</h2>
+      <div className="info-line">
+        Born: {birth_year}, Height: {height}cm, Weight: {mass}kg
+      </div>
+      <div className="info">
+        <p>
+          {name} is from {homeworldState ? homeworldState.name : "Loading"}. {gender === "male" ? "He" : (gender === "female" ? "She" : "They")} appeared in {films.length} film{films.length === 1 ? "" : "s"}.
+        </p>
+      </div>
+    <div></div>
     </div>
   );
 };
